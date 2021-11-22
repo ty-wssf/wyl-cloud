83e82bf0bd4685c3b60d35b3c5a179b646aa5cbb
wyl <791128058@qq.com>
2021-11-22 21:52:40 +0800

    ...

---
+++ wyl-modules/dict/dict-client/src/main/java/com/wyl/dto/ConfigPageQry.java
@@ -0,0 +1,10 @@
+package com.wyl.dto;
+
+import com.alibaba.cola.dto.PageQuery;
+
+/**
+ * @author wyl
+ * @since 2021-11-22 17:53:50
+ */
+public class ConfigPageQry extends PageQuery {
+}
--- wyl-modules/dict/dict-client/src/main/java/com/wyl/dto/SysConfigQry.java
+++
@@ -1,8 +0,0 @@
-package com.wyl.dto;
-
-/**
- * @author wyl
- * @since 2021-11-22 17:53:50
- */
-public class SysConfigQry {
-}
