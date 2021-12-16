38eab722a4e754c3e4d1632c8323da42c4e521e2
wyl <791128058@qq.com>
2021-12-16 16:57:33 +0800

    feat: EasyCode配置提交

--- wyl-modules/dict/dict-adapter/src/main/java/com/wyl/dict/web/SysNoticeController.java
+++ wyl-modules/dict/dict-adapter/src/main/java/com/wyl/dict/web/SysNoticeController.java
@@ -1,13 +1,16 @@
 package com.wyl.dict.web;
 
+import cn.wyl.common.core.dto.MultiResponse;
 import cn.wyl.common.core.dto.PageResponse;
 import cn.wyl.common.core.dto.Response;
 import cn.wyl.common.core.dto.SingleResponse;
+import cn.wyl.common.core.utils.poi.ExcelUtil;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.wyl.dict.dto.clientobject.SysNoticeCO;
 import com.wyl.dict.dto.command.SysNoticeAddCommand;
 import com.wyl.dict.dto.command.SysNoticeEditCommand;
 import com.wyl.dict.dto.qry.SysNoticePageQry;
+import com.wyl.dict.dto.qry.SysNoticeQry;
 import com.wyl.dict.service.SysNoticeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,12 +19,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 通知公告表(SysNotice)表控制层
  *
  * @author wyl
- * @since 2021-12-15 09:41:50
+ * @since 2021-12-16 15:50:05
  */
 @Api(tags = "通知公告表模块")
 @RestController
@@ -70,6 +74,15 @@ public class SysNoticeController {
     public Response remove(@ApiParam(value = "主键", required = true)
                            @PathVariable Integer[] ids) {
         return this.sysNoticeService.deleteByPrimaryKeys(ids);
+    }
+
+    @ApiOperationSupport(order = 6)
+    @ApiOperation(value = "导出")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysNoticeQry qry) {
+        MultiResponse<SysNoticeCO> list = sysNoticeService.queryAll(qry);
+        ExcelUtil<SysNoticeCO> util = new ExcelUtil<SysNoticeCO>(SysNoticeCO.class);
+        util.exportExcel(response, list.getData(), "通知公告表数据");
     }
     /* 基础接口结束 */
 
--- wyl-modules/dict/dict-client/src/main/java/com/wyl/dict/dto/command/SysNoticeAddCommand.java
+++ wyl-modules/dict/dict-client/src/main/java/com/wyl/dict/dto/command/SysNoticeAddCommand.java
@@ -1,56 +1,77 @@
 package com.wyl.dict.dto.command;
 
 import cn.wyl.common.core.dto.Command;
-import lombok.Data;
-import java.util.Date;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 /**
  * 通知公告表(SysNotice)查询对象
  *
  * @author wyl
- * @since 2021-12-15 09:06:44
+ * @since 2021-12-16 15:38:57
  */
 @ApiModel("新增通知公告表模型")
 @Data
 public class SysNoticeAddCommand extends Command {
-    
+
     /**
-    * 公告标题
-    */    @ApiModelProperty(value = "公告标题", position = 2)
+     * 公告标题
+     */
+    @ApiModelProperty(value = "公告标题", position = 2)
+    @NotBlank(message = "公告标题不能为空")
+    @Size(max = 64, message = "公告标题长度不能超过{max}个字符")
     private String noticeTitle;
     /**
-    * 公告类型（1通知 2公告）
-    */    @ApiModelProperty(value = "公告类型（1通知 2公告）", position = 3)
+     * 公告类型（1通知 2公告）
+     */
+    @ApiModelProperty(value = "公告类型（1通知 2公告）", position = 3)
+    @NotBlank(message = "公告类型（1通知 2公告）不能为空")
+    @Size(max = 1, message = "公告类型（1通知 2公告）长度不能超过{max}个字符")
     private String noticeType;
     /**
-    * 公告内容
-    */    @ApiModelProperty(value = "公告内容", position = 4)
+     * 公告内容
+     */
+    @ApiModelProperty(value = "公告内容", position = 4)
+    @NotBlank(message = "公告内容不能为空")
     private String noticeContent;
     /**
-    * 公告状态（0正常 1关闭）
-    */    @ApiModelProperty(value = "公告状态（0正常 1关闭）", position = 5)
+     * 公告状态（0正常 1关闭）
+     */
+    @ApiModelProperty(value = "公告状态（0正常 1关闭）", position = 5)
+    @Size(max = 1, message = "公告状态（0正常 1关闭）长度不能超过{max}个字符")
     private String status;
     /**
-    * 创建者
-    */    @ApiModelProperty(value = "创建者", position = 6)
+     * 创建者
+     */
+    @ApiModelProperty(value = "创建者", position = 6)
+    @Size(max = 64, message = "创建者长度不能超过{max}个字符")
     private String createBy;
     /**
-    * 创建时间
-    */    @ApiModelProperty(value = "创建时间", position = 7)
+     * 创建时间
+     */
+    @ApiModelProperty(value = "创建时间", position = 7)
     private Date createTime;
     /**
-    * 更新者
-    */    @ApiModelProperty(value = "更新者", position = 8)
+     * 更新者
+     */
+    @ApiModelProperty(value = "更新者", position = 8)
+    @Size(max = 64, message = "更新者长度不能超过{max}个字符")
     private String updateBy;
     /**
-    * 更新时间
-    */    @ApiModelProperty(value = "更新时间", position = 9)
+     * 更新时间
+     */
+    @ApiModelProperty(value = "更新时间", position = 9)
     private Date updateTime;
     /**
-    * 备注
-    */    @ApiModelProperty(value = "备注", position = 10)
+     * 备注
+     */
+    @ApiModelProperty(value = "备注", position = 10)
+    @Size(max = 256, message = "备注长度不能超过{max}个字符")
     private String remark;
 
 }
