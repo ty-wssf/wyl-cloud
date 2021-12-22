4fc6654d9ea7d77dd9e57d1757d1aad009f4031b
wyl <791128058@qq.com>
2021-12-22 15:56:07 +0800

    feat: 。。。

--- wyl-ui/src/views/system/role/index.vue
+++ wyl-ui/src/views/system/role/index.vue
@@ -82,57 +82,53 @@
       @pagination="pageList"
     />
 
-    <!-- 添加或修改参数配置对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="780px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="角色名称" prop="roleName">
-              <el-input v-model="form.roleName" placeholder="请输入角色名称"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="角色权限字符串" prop="roleKey">
-              <el-input v-model="form.roleKey" placeholder="请输入角色权限字符串"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="显示顺序" prop="roleSort">
-              <el-input v-model="form.roleSort" placeholder="请输入显示顺序"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）" prop="dataScope">
-              <el-input v-model="form.dataScope"
-                        placeholder="请输入数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="菜单树选择项是否关联显示" prop="menuCheckStrictly">
-              <el-input v-model="form.menuCheckStrictly" placeholder="请输入菜单树选择项是否关联显示"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="部门树选择项是否关联显示" prop="deptCheckStrictly">
-              <el-input v-model="form.deptCheckStrictly" placeholder="请输入部门树选择项是否关联显示"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="角色状态（0正常 1停用）" prop="status">
-              <el-input v-model="form.status" placeholder="请输入角色状态（0正常 1停用）"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="删除标志（0代表存在 2代表删除）" prop="delFlag">
-              <el-input v-model="form.delFlag" placeholder="请输入删除标志（0代表存在 2代表删除）"/>
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="备注" prop="remark">
-              <el-input v-model="form.remark" placeholder="请输入备注"/>
-            </el-form-item>
-          </el-col>
-        </el-row>
+    <!-- 添加或修改角色配置对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="form.roleName" placeholder="请输入角色名称"/>
+        </el-form-item>
+        <el-form-item prop="roleKey">
+          <span slot="label">
+            <el-tooltip content="控制器中定义的权限字符，如：@PreAuthorize(`@ss.hasRole('admin')`)" placement="top">
+              <i class="el-icon-question"></i>
+            </el-tooltip>
+            权限字符
+          </span>
+          <el-input v-model="form.roleKey" placeholder="请输入权限字符"/>
+        </el-form-item>
+        <el-form-item label="角色顺序" prop="roleSort">
+          <el-input-number v-model="form.roleSort" controls-position="right" :min="0"/>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.status">
+            <el-radio
+              v-for="dict in dict.type.sys_normal_disable"
+              :key="dict.value"
+              :label="dict.value"
+            >{{dict.label}}
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="菜单权限">
+          <el-checkbox v-model="menuExpand" @change="handleCheckedTreeExpand($event, 'menu')">展开/折叠</el-checkbox>
+          <el-checkbox v-model="menuNodeAll" @change="handleCheckedTreeNodeAll($event, 'menu')">全选/全不选</el-checkbox>
+          <el-checkbox v-model="form.menuCheckStrictly">父子联动
+          </el-checkbox>
+          <el-tree
+            class="tree-border"
+            :data="menuOptions"
+            show-checkbox
+            ref="menu"
+            node-key="id"
+            :check-strictly="!form.menuCheckStrictly"
+            empty-text="加载中，请稍候"
+            :props="{'label': 'menuName', 'children': 'children'}"
+          ></el-tree>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容"></el-input>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -144,9 +140,11 @@
 
 <script>
   import {pageList, getInfo, add, edit, remove} from "@/api/system/role";
+  import {treeList as menuTreeList} from "@/api/system/menu";
 
   export default {
     name: "sysRole",
+    dicts: ['sys_normal_disable'],
     data() {
       return {
         // 遮罩层
@@ -179,11 +177,34 @@
         // 表单参数
         form: {},
         // 表单校验
-        rules: {}
+        rules: {
+          roleName: [
+            {required: true, message: "角色名称不能为空", trigger: "blur"}
+          ],
+          roleKey: [
+            {required: true, message: "权限字符不能为空", trigger: "blur"}
+          ],
+          roleSort: [
+            {required: true, message: "角色顺序不能为空", trigger: "blur"}
+          ]
+        },
+        // 菜单列表
+        menuOptions: [],
+        // 部门列表
+        deptOptions: [],
+        // 菜单是否展开
+        menuExpand: false,
+        // 菜单全选
+        menuNodeAll: false,
+        // 部门展开
+        deptExpand: true,
+        // 部门全选
+        deptNodeAll: false,
       };
     },
     created() {
       this.pageList();
+      this.menuTreeList();
     },
     methods: {
       /** 查询参数列表 */
@@ -209,17 +230,12 @@
       // 表单重置
       reset() {
         this.form = {
-          roleId: undefined,
-          roleName: undefined,
-          roleKey: undefined,
-          roleSort: undefined,
-          dataScope: undefined,
-          menuCheckStrictly: undefined,
-          deptCheckStrictly: undefined,
-          status: undefined,
-          delFlag: undefined,
-          remark: undefined,
-          remark: undefined
+          roleSort: 0,
+          status: "0",
+          menuIds: [],
+          deptIds: [],
+          menuCheckStrictly: true,
+          deptCheckStrictly: true
         };
         this.resetForm("form");
       },
@@ -246,6 +262,23 @@
       },
       /** 提交按钮 */
       submitForm: function () {
+        this.$refs["form"].validate(valid => {
+          if (valid) {
+            if (this.form.roleId != undefined) {
+              edit(this.form.roleId, this.form).then(response => {
+                this.$modal.msgSuccess("修改成功");
+                this.open = false;
+                this.pageList();
+              });
+            } else {
+              add(this.form).then(response => {
+                this.$modal.msgSuccess("新增成功");
+                this.open = false;
+                this.pageList();
+              });
+            }
+          }
+        });
       },
       /** 删除按钮操作 */
       handleDelete(row) {
@@ -254,6 +287,34 @@
       /** 导出按钮操作 */
       handleExport() {
 
+      },
+      /** 查询菜单树结构 */
+      menuTreeList() {
+        menuTreeList().then(response => {
+          this.menuOptions = response.data;
+        });
+      },
+      // 树权限（展开/折叠）
+      handleCheckedTreeExpand(value, type) {
+        if (type == 'menu') {
+          let treeList = this.menuOptions;
+          for (let i = 0; i < treeList.length; i++) {
+            this.$refs.menu.store.nodesMap[treeList[i].id].expanded = value;
+          }
+        } else if (type == 'dept') {
+          let treeList = this.deptOptions;
+          for (let i = 0; i < treeList.length; i++) {
+            this.$refs.dept.store.nodesMap[treeList[i].id].expanded = value;
+          }
+        }
+      },
+      // 树权限（全选/全不选）
+      handleCheckedTreeNodeAll(value, type) {
+        if (type == 'menu') {
+          this.$refs.menu.setCheckedNodes(value ? this.menuOptions : []);
+        } else if (type == 'dept') {
+          this.$refs.dept.setCheckedNodes(value ? this.deptOptions : []);
+        }
       },
     }
   };
